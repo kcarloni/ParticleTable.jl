725a889126bf9c244a3c85c5159f514fc3c1821b
kcarloni <kcarloni@g.harvard.edu>
2025-07-09 14:31:55 -0400

    since Corpuscles doesn't support nuclei yet, hack it together with IsotopeTable

--- src/ParticleTable.jl
+++ src/ParticleTable.jl
@@ -1,8 +1,54 @@
 module ParticleTable
 
-using PyCall
 using Corpuscles
+using Corpuscles: pdgid 
+using IsotopeTable # Corpuscles doesn't have nuclear isotope functionality yet
 
+export Particle
+export Isotope
+export pdgid
+
+Base.broadcastable( p::Particle ) = Ref(p)
+
+# ====================
+# functionality for nuclei
+
+function Base.getproperty( N::Isotope, sym::Symbol )
+
+    if sym in fieldnames( Isotope )
+        return getfield( N, sym )
+    elseif sym == :A
+        return N.mass_number
+    elseif sym == :Z
+        return N.atomic_number
+    end
+
+end
+
+Corpuscles.pdgid(; A, Z, L=0, I=0 ) = PDGID( Int( 1e9 + L * 1e5 + Z * 1e4 + A * 10 + I ) )
+Corpuscles.pdgid( N::Isotope ) = Corpuscles.pdgid(; A=N.A, Z=N.Z )
+
+function Corpuscles.Particle(; A, Z )
+    return isotopes(Z,A)
+end
+
+# let's overwrite this to handle nuclei for the time being...
+function Corpuscles.Particle( id::Corpuscles.ParticleID ) 
+    
+    if isnucleus( id )
+        return Particle(; A=Corpuscles.A(id), Z=Corpuscles.Z(id) )
+    else
+        # default 
+        return catalog.particle_dict[Base.convert(PDGID, id)]
+    end
+
+end
+
+# ====================
+# functionality to print particle decays (very useful);
+# from Anatoli Feydnitch's `particletools` Python package
+
+using PyCall 
 const print_decay_channels = PyNULL()
 # const PythiaTable = PyNull()
 
@@ -24,11 +70,5 @@ function decay_modes( p::Particle )
     print_decay_channels( p.pdgid.value )
 end
 export decay_modes
-
-import Base: broadcastable
-
-broadcastable( p::Particle ) = Ref(p)
-
-export Particle
 
 end # module ParticleTable
